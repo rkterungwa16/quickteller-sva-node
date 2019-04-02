75045c7c1eba459e90e1a432bba18d718bdbc301
rkterungwa16 <terungwakombol@gmail.com>
2019-04-02 16:30:08 +0100

    Implement get billers by category

--- src/BillersResource.js
+++ src/BillersResource.js
@@ -19,11 +19,34 @@ class BillersResource {
   }
 
   /**
-   * @return {Promise} get billers i
+   * @return {Promise} return billers
    */
   getBillers () {
     const method = 'GET'
     const path = '/api/v2/quickteller/billers'
+    return new Promise((resolve, reject) => {
+      new HttpClient({
+        clientId: this.clientId,
+        apiSecret: this.apiSecret,
+        terminalId: this.terminalId
+      }, {
+        protocol: this.protocol,
+        hostname: this.hostname,
+        path,
+        method
+      }).sendRequest()
+        .then(response => resolve(response))
+        .catch(err => reject(err))
+    })
+  }
+
+  /**
+   * @param {number} categoryId category id
+   * @return {Promise} return billers by category
+   */
+  getBillersByCategory (categoryId) {
+    const method = 'GET'
+    const path = `/api/v2/quickteller/categorys/${categoryId}/billers`
     return new Promise((resolve, reject) => {
       new HttpClient({
         clientId: this.clientId,
