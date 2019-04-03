41fe8b9653e138443bf0330c073ff6360d842e7f
rkterungwa16 <terungwakombol@gmail.com>
2019-04-03 11:21:46 +0100

    Implement payment enquiry feature

---
+++ examples/paymentEnquiry.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv'
+import QuickTellerSva from '../src/QuickTellerSva'
+
+dotenv.config()
+
+const quickTellerSva = new QuickTellerSva(
+  {
+    apiSecret: process.env.API_SECRET,
+    clientId: process.env.CLIENT_ID,
+    terminalId: process.env.TERMINAL_ID
+  }
+)
+
+quickTellerSva
+  .TransactionResource
+  .paymentEnquiry({
+    paymentCode: '04226901',
+    customerId: '3394433',
+    customerMobile: '08032269223',
+    customerEmail: 'customer1@gmail.com',
+    pageFlowValues: 'BankId:16|DestinationAccountNumber:0221149201|DestinationAccountType:10|Amount:60000|ReciepientName:GBOLAHAN MUSBAU SUBAIR',
+    amount: '60000'
+  })
+  .then((response) => {
+    console.log('response', response)
+  })
+  .catch((err) => {
+    console.log('error', err)
+  })
--- src/TransactionResource.js
+++ src/TransactionResource.js
@@ -21,8 +21,8 @@ class TransactionResource {
   /**
    * Retrieve the status of a transaction
    * @param {string} requestReferencevalue The request reference passed in the
-   * 'SendBillPaymentAdvice' method or the transfer code passed in the 'DoTransfer'
-   * method
+   * 'SendBillPaymentAdvice' method or the transfer code
+   * passed in the 'DoTransfer' method
    * @return {Promise} return  biller categories
    */
   queryTransaction (requestReferencevalue) {
@@ -38,6 +38,43 @@ class TransactionResource {
         hostname: this.hostname,
         path,
         method
+      }).sendRequest()
+        .then(response => resolve(response))
+        .catch(err => reject(err))
+    })
+  }
+
+  /**
+   * Quickteller Payment Inquiry
+   * @param {object} requestPayload - request payload
+   * @param {string} requestPayload.paymentCode - The Payment Code that identifies
+   * the biller. Please see Get Biller response GetBillerPaymentItems call
+   * @param {string} requestPayload.customerId - Customer ID e.g. smart card number
+   * meter number
+   * @param {string} [requestPayload.customerMobile] - Customer's Mobile Number
+   * @param {string} [requestPayload.customerEmail] - Customer's Email
+   * @param {amount} [requestPayload.amount] - Amount
+   * be sent in lower denomination
+   * @param {string} requestPayload.pageFlowValues - This is required
+   * when you are sending a funds transfer trasaction. e.
+   * BankId:16|DestinationAccountNumber:0221149201|
+   * DestinationAccountType:10|Amount:60000|ReciepientName:GBOLAHAN MUSBAU SUBAIR
+   * @return {Promise} return payment information
+   */
+  paymentEnquiry (requestPayload) {
+    const method = 'POST'
+    const path = '/api/v2/quickteller/transactions/inquirys'
+    return new Promise((resolve, reject) => {
+      new HttpClient({
+        clientId: this.clientId,
+        apiSecret: this.apiSecret,
+        terminalId: this.terminalId
+      }, {
+        protocol: this.protocol,
+        hostname: this.hostname,
+        path,
+        method,
+        requestPayload
       }).sendRequest()
         .then(response => resolve(response))
         .catch(err => reject(err))
